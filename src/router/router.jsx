df4acaf8826663c2794ad56279bbd958e348eb55
mariandrean <m.andreaan@gmail.com>
2024-02-07 12:07:25 +0100

    fix router

--- src/router/router.jsx
+++ src/router/router.jsx
@@ -1,5 +1,5 @@
 import { createBrowserRouter } from "react-router-dom";
-import Home from "../components/Contact"
+import Home from "../components/Home"
 import Contact from "../components/Contact";
 
 
