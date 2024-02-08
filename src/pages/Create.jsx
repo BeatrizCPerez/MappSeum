049d4c2c453a4fdd9bafafcf7b2172749057ae65
Leite <leite.way@gmail.com>
2024-02-08 14:42:21 +0100

    the structure and style of the form is ready, we change the button add and added mediaqueries for this page

--- src/pages/Create.jsx
+++ src/pages/Create.jsx
@@ -1,27 +1,36 @@
 import React from 'react'
+import './Form.css'
 
 const Create = () => {
   return (
     <div>
-    <h1>AÑADE UNA NUEVA OBRA</h1>
-    <form>
+    <form className="container-form">
+      <h1>AÑADE UNA NUEVA OBRA</h1>
     <label>Obra:</label>
-    <input type="text" name="obra" placeholder='enter name of your sculpture'/>
+    <input type="text" name="obra" placeholder='Escribe el nombre de la obra'/>
+    <hr></hr>
 
     <label>Autor:</label>
-    <input type="text" name="autor" placeholder="enter autor's name"/>
+    <input type="text" name="autor" placeholder="Escribe el nombre de Autor"/>
+    <hr></hr>
 
     <label>Año:</label>
-    <input type="text" name="año" placeholder="enter the year of the sculpture"/>
+    <input type="text" name="año" placeholder="Escribe el año de creación"/>
+    <hr></hr>
 
     <label>Material:</label>
-    <input type="text" name="material" placeholder="enter the material of the sculpture"/>
+    <input type="text" name="material" placeholder="Escribe el tipo de material de tu escultura"/>
+    <hr></hr>
 
     <label>Ubicación:</label>
-    <input type="text" name="ubicacion" placeholder="enter the location of the sculpture"/>
+    <input type="text" name="ubicacion" placeholder="Escribe la ubicación de tu escultura"/>
+    <hr></hr>
 
     <label>Imagen de la Escultura:</label>
-    <input type="file" name="imagen" accept="image/*"/>
+    {/* <input className="img-file" type="file" name="imagen" accept="image/*"/> */}
+    <input  placeholder="Escribe el link de tu imagen" type="link" name="imagen" accept="image/*"/>
+
+    <button type="submit">Agregar</button>
 
 
   </form>
