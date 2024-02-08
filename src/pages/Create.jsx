3aad057b036953b75aa7c71624378043a5aa233e
Leite <leite.way@gmail.com>
2024-02-08 14:57:14 +0100

    delete the hr line

--- src/pages/Create.jsx
+++ src/pages/Create.jsx
@@ -8,23 +8,18 @@ const Create = () => {
       <h1>AÑADE UNA NUEVA OBRA</h1>
     <label>Obra:</label>
     <input type="text" name="obra" placeholder='Escribe el nombre de la obra'/>
-    <hr></hr>
 
     <label>Autor:</label>
     <input type="text" name="autor" placeholder="Escribe el nombre de Autor"/>
-    <hr></hr>
 
     <label>Año:</label>
     <input type="text" name="año" placeholder="Escribe el año de creación"/>
-    <hr></hr>
 
     <label>Material:</label>
     <input type="text" name="material" placeholder="Escribe el tipo de material de tu escultura"/>
-    <hr></hr>
 
     <label>Ubicación:</label>
     <input type="text" name="ubicacion" placeholder="Escribe la ubicación de tu escultura"/>
-    <hr></hr>
 
     <label>Imagen de la Escultura:</label>
     {/* <input className="img-file" type="file" name="imagen" accept="image/*"/> */}
