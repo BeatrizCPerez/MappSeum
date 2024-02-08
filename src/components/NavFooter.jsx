b1f3003335f0e1bd650c4b87f992e873531214fa
mariandrean <m.andreaan@gmail.com>
2024-02-08 13:54:00 +0100

    fixing navbar, adding footer

--- src/components/NavFooter.jsx
+++ src/components/NavFooter.jsx
@@ -1,13 +1,27 @@
 import React from 'react'
 import "./NavFooter.css"
+import { Outlet } from 'react-router-dom'
 
 const NavFooter = () => {
   return (
+    <>
     <nav className="menu">
-      <a><img src="src\assets\logo.png"/></a>
+      <a href="/"><img src="src\assets\mappseum-logo.png"/></a>
       <button>Agregar escultura</button>
     </nav>
-  )
+    <Outlet/>
+    <footer>
+      <p>© 2024 MappSeum</p>
+      <div>
+        <a href='https://www.facebook.com/' target='blank'><img src="src\assets\logos-redes\Facebook.png"/></a>
+        <a href="https://www.instagram.com/" target='blank'><img src="src\assets\logos-redes\Instagram.png"/></a>
+        <a href="https://github.com/BeatrizCPerez/MappSeum" target='blank'><img src="src\assets\logos-redes\GitHub.png"/></a>
+      </div>
+    </footer>
+   </>    
+  )    
+ 
+
 }
 
 export default NavFooter
