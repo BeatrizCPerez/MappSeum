ebd0ff8c4fcc285e32e308e9765c57ce2079422a
mariandrean <m.andreaan@gmail.com>
2024-02-17 17:30:55 +0100

    puting edit form inputs within each label

--- src/components/FormEdit.jsx
+++ src/components/FormEdit.jsx
@@ -36,28 +36,34 @@ const FormEdit = () => {
     <>
     
         <form onSubmit = {handleSubmit(editedSculpture => {editSculptures(); reset()})} className="container-form">
-          <label>Obra:</label>
-          <input {...register("title", {required: "El campo obra esta vacio"})} type="text" placeholder='Escribe el nombre de la obra' />
-          {errors.title && <div className="text-error">{errors.title.message}</div>}
+          <label>Obra:
+            <input {...register("title", {required: "El campo obra esta vacio"})} type="text" placeholder='Escribe el nombre de la obra' />
+            {errors.title && <div className="text-error">{errors.title.message}</div>}
+          </label>
   
-          <label>Autor:</label>
-          <input {...register("author", {required: true})} type="text" placeholder="Escribe el nombre de Autor" />
-  
-          <label>Año:</label>
-          <input {...register("year", {validate: validateNumber})} type="text" placeholder="Escribe el año de creación" />
-          {errors.year && <div className="text-error">{errors.year.message}</div>}
-  
-          <label>Material:</label>
-          <input {...register("material")} type="text" placeholder="Escribe el tipo de material" />
-  
-          <label>Ubicación:</label>
-          <input {...register("location")} type="text" placeholder="Escribe la ubicación" />
-  
-          <label>Imagen de la Escultura:</label>
-          <input {...register("imageUrl", {validate: validateLink, required: "Hace falta un link"})} placeholder="Escribe el link de tu imagen" type="link"/>
-          {errors.imageUrl && <div className="text-error">{errors.imageUrl.message}</div>}
-          {/* <input className="img-file" type="file" name="imagen" accept="image/*"/> */}
+          <label>Autor:
+            <input {...register("author", {required: true})} type="text" placeholder="Escribe el nombre de Autor" />
+          </label>
 
+          <label>Año:
+            <input {...register("year", {validate: validateNumber})} type="text" placeholder="Escribe el año de creación" />
+            {errors.year && <div className="text-error">{errors.year.message}</div>}
+          </label>
+  
+          <label>Material:
+            <input {...register("material")} type="text" placeholder="Escribe el tipo de material" />
+          </label>
+  
+          <label>Ubicación:
+            <input {...register("location")} type="text" placeholder="Escribe la ubicación" />
+          </label>
+  
+          <label>Imagen de la Escultura:
+            <input {...register("imageUrl", {validate: validateLink, required: "Hace falta un link"})} placeholder="Escribe el link de tu imagen" type="link"/>
+            {errors.imageUrl && <div className="text-error">{errors.imageUrl.message}</div>}
+            {/* <input className="img-file" type="file" name="imagen" accept="image/*"/> */}
+          </label>
+          
           <div className="buttons-container">
           <button type="submit">Guardar</button>
           <Link to="/">
