f4fff313334e2b6f18a1d7dee80734815f0d7059
krisneiras <kristygns21@gmail.com>
2024-02-26 11:52:04 +0100

    finishing sweet alerts

--- src/services/sculptures-services.js
+++ src/services/sculptures-services.js
@@ -15,11 +15,47 @@ export const getSculptureById = async (id) => {
 }
 
 // DELETE
-export const deleteSculptures = async (id) => {
+/* export const deleteSculptures = async (id) => {
     if (confirm("¿Estás seguro que quieres eliminar?") === true) {
         const response = await fetch(`http://localhost:3000/sculptures/${id}`, {
             method: 'DELETE'
         });
+    }
+}; */
+
+export const deleteSculptures = async (id) => {
+    const result = await Swal.fire({
+        title: '¿Estás seguro que quieres eliminar?',
+        text: "¡No podrás revertir esto!",
+        icon: 'warning',
+        showCancelButton: true,
+        confirmButtonColor: '#3085d6',
+        cancelButtonColor: '#d33',
+        confirmButtonText: 'Sí, eliminarlo',
+        cancelButtonText: 'Cancelar'
+    });
+
+    if (result.isConfirmed) {
+        const response = await fetch(`http://localhost:3000/sculptures/${id}`, {
+            method: 'DELETE'
+        });
+
+        if (response.ok) {
+            Swal.fire(
+                '¡Eliminado!',
+                'La escultura ha sido eliminada correctamente.',
+                'success'
+            ).then(() => {
+                // Recargar la página después de eliminar la escultura
+                window.location.reload();
+            });
+        } else {
+            Swal.fire(
+                'Error',
+                'Hubo un problema al intentar eliminar la escultura.',
+                'error'
+            );
+        }
     }
 };
 
@@ -54,7 +90,20 @@ export const editSculptures = async (id, data) => {
         },
         body: JSON.stringify(data)
     });
+
+    if (response.ok) {
+        Swal.fire(
+            '¡Editado!',
+            'Tu escultura ha sido editada con éxito.',
+            'success'
+        );
+    } else {
+        Swal.fire(
+            'Error',
+            'Hubo un problema al intentar editar la escultura.',
+            'error'
+        );
+    }
+
     return response;
-}
-
-
+};
