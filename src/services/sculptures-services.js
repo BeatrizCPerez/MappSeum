a52f26c84215203503e53fc13b55c275cdaf77d3
krisneiras <kristygns21@gmail.com>
2024-02-26 12:24:23 +0100

    adding sweetalert to dbjson petitions

--- src/services/sculptures-services.js
+++ src/services/sculptures-services.js
@@ -1,3 +1,5 @@
+import Swal from 'sweetalert2';
+
 // GET
 export const getSculptures = async () => {
     const response = await fetch('http://localhost:3000/sculptures');
@@ -13,17 +15,53 @@ export const getSculptureById = async (id) => {
 }
 
 // DELETE
-export const deleteSculptures = async (id) => {
+/* export const deleteSculptures = async (id) => {
     if (confirm("¿Estás seguro que quieres eliminar?") === true) {
         const response = await fetch(`http://localhost:3000/sculptures/${id}`, {
             method: 'DELETE'
         });
     }
+}; */
+
+export const deleteSculptures = async (id) => {
+    const result = await Swal.fire({
+        title: '¿Estás seguro que quieres eliminar?',
+        text: "¡No podrás revertir esto!",
+        icon: 'warning',
+        showCancelButton: true,
+        confirmButtonColor: '#3085d6',
+        cancelButtonColor: '#d33',
+        confirmButtonText: 'Sí, eliminarlo',
+        cancelButtonText: 'Cancelar'
+    });
+
+    if (result.isConfirmed) {
+        const response = await fetch(`http://localhost:3000/sculptures/${id}`, {
+            method: 'DELETE'
+        });
+
+        if (response.ok) {
+            Swal.fire(
+                '¡Eliminado!',
+                'La escultura ha sido eliminada correctamente.',
+                'success'
+            ).then(() => {
+                // Recargar la página después de eliminar la escultura
+                window.location.reload();
+            });
+        } else {
+            Swal.fire(
+                'Error',
+                'Hubo un problema al intentar eliminar la escultura.',
+                'error'
+            );
+        }
+    }
 };
 
 // POST
 export const createSculptures = async (newSculptures) => {
-    alert("Obra creada con éxito!")
+    Swal.fire("Obra creada con éxito!");
     const response = await fetch('http://localhost:3000/sculptures', {
         method: 'POST',
         headers: {
@@ -52,7 +90,20 @@ export const editSculptures = async (id, data) => {
         },
         body: JSON.stringify(data)
     });
+
+    if (response.ok) {
+        Swal.fire(
+            '¡Editado!',
+            'Tu escultura ha sido editada con éxito.',
+            'success'
+        );
+    } else {
+        Swal.fire(
+            'Error',
+            'Hubo un problema al intentar editar la escultura.',
+            'error'
+        );
+    }
+
     return response;
-}
-
-
+};
