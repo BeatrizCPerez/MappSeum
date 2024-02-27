1a5a394cbf3a66fdef780cef8127ad3f5768405d
krisneiras <kristygns21@gmail.com>
2024-02-27 14:06:49 +0100

    Form tests: checking if button exists

--- src/components/NavFooter.test.jsx
+++ src/components/NavFooter.test.jsx
@@ -1,4 +1,3 @@
-// Las funciones de test, comprenden de dos parametros, el nombre del test y el codigo de mi test
 import { render, screen } from "@testing-library/react";
 import NavFooter from "./NavFooter";
 import { expect } from "vitest";
