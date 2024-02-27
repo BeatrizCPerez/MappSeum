b7f6f6996a37b9256d71bb079023f2340e88c560
krisneiras <kristygns21@gmail.com>
2024-02-27 11:49:12 +0100

    Adding test to navfooter component

--- src/components/NavFooter.test.jsx
+++ src/components/NavFooter.test.jsx
@@ -1,25 +1,12 @@
 // Las funciones de test, comprenden de dos parametros, el nombre del test y el codigo de mi test
-import { fireEvent, render, screen } from "@testing-library/react";
+import { render, screen } from "@testing-library/react";
 import NavFooter from "./NavFooter";
 import { expect } from "vitest";
+import { MemoryRouter } from "react-router";
 
-// metodo - primer parametro - funcion anonima, pot tanto directamente comienza con una funcion flecha
-test("render NavFooter", () => {
-    //llamamos al renderizado e importamos la funcion 
-    render(<NavFooter/>)
+  test("textoFooter", () => {
+    render(<MemoryRouter><NavFooter/></MemoryRouter>)
 
-    const countElement = screen.getByText(/count is/i)
-
-    expect(countElement).toBeDefined();
-})
-
-
-test("should rise when buton pressed"), () => {
-    render(<App/>)
-
-    const countElement = screen.getByText(/count is/i)
-
-    fireEvent.click(countElement)
-    const increaseNumber = screen.getByText(/1/i)
-    expect(increaseNumber).toBeDefined();
-}
+    const texto = screen.getByText(/MappSeum/i);
+    expect(texto).toBeDefined();
+  })
